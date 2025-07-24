42047b0bff89e7ab8c71fa354ec6d81e1e4d9e3c
Athmakuri_Akshara <107808096+AksharaAthmakuri@users.noreply.github.com>
2025-07-24 19:26:05 +0530

    Add files via upload
    Creating a Book Class Program in Java

---
+++ Task1.java
@@ -0,0 +1,29 @@
+package javaoops_polymorphism;
+
+class Book{
+	String title;
+	String author;
+	int price;
+	
+	Book(String title,String author,int price)
+	{
+		this.title=title;
+		this.author=author;
+		this.price=price;
+	}
+	void displayDetails()
+	{
+		System.out.println("the title is:"+title+"   "+"the author is :" +author+"   "+"the price is:"+price);
+	}
+}
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book b=new Book("lifes amazing secrets","gaur gopal das",169);
+		b.displayDetails();
+
+	}
+
+}
