c8707e5a137f987792bb13a0d975744f7cfac707
Athmakuri_Akshara <107808096+AksharaAthmakuri@users.noreply.github.com>
2025-07-25 22:08:42 +0530

    Create Package1_Main.java

---
+++ Packages_Program1/Package1_Main.java
@@ -0,0 +1,15 @@
+package executionp;
+import com.school.student.*;
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s=new Student(2,"bob",15);
+		Student s1=new Student(3,"riya",22);
+		Student s2=new Student(4,"yug",16);
+		Student s3=new Student(5,"alex",14);
+		s.display();
+		s1.display();
+		s2.display();
+		s3.display();
+	}
+}
