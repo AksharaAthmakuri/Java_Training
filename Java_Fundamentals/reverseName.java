4311345243668408960d727e6d8ddec55be9ac29
Athmakuri_Akshara <107808096+AksharaAthmakuri@users.noreply.github.com>
2025-07-25 20:42:22 +0530

    Create reverseName.java

---
+++ Java_Fundamentals/reverseName.java
@@ -0,0 +1,24 @@
+
+public class Task52 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(args.length>0)
+		{
+			String input=args[0];
+			String rev="";
+			for(int i=input.length()-1;i>=0;i--)
+			{
+				rev+=input.charAt(i);
+			}
+			
+			System.out.println(" reversed:"+rev);
+		}
+		else
+		{
+			System.out.println("please provide as comdline arg");
+		}
+
+	}
+
+}
