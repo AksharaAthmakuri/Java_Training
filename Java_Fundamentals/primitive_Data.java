276303527e1d747ad9b9bef81fc1d2a145e9f35d
Athmakuri_Akshara <107808096+AksharaAthmakuri@users.noreply.github.com>
2025-07-25 20:52:11 +0530

    Create primitive_Data.java

---
+++ Java_Fundamentals/primitive_Data.java
@@ -0,0 +1,22 @@
+package arraysjava;
+public class Task71 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a=100;
+		short b=1200;
+		int c=123000;
+		long d=10000000l;
+		float f=5.65f;
+		double e=23.43;
+		char g='A';
+		boolean h=true;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(e);
+		System.out.println(g);
+		System.out.println(h);
+	}
+}
