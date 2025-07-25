daa8002f5ee5860bc9581f3d88c7f56cae210950
Athmakuri_Akshara <107808096+AksharaAthmakuri@users.noreply.github.com>
2025-07-25 20:32:02 +0530

    Create Print_Statements.java

---
+++ Java_Fundamentals/Print_Statements.java
@@ -0,0 +1,12 @@
+
+public class Task31 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		System.out.println("my name Akshara");
+		System.out.println("Today is a great day to code Java!");
+
+	}
+
+}
