c327dc94cd3f1887322365ed487df01a5d4e8f14
Athmakuri_Akshara <107808096+AksharaAthmakuri@users.noreply.github.com>
2025-07-25 21:13:43 +0530

    Create Control_stm4.java

---
+++ Java_Fundamentals/Control_stm4.java
@@ -0,0 +1,16 @@
+package arraysjava;
+import java.util.Scanner;
+public class Task94 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan =new Scanner(System.in);
+		int n;
+		do
+		{
+			System.out.println("enter the positive num(-1 to stop) :");
+			 n=scan.nextInt();
+		}
+		while(n!=-1);
+		System.out.println("program ended");
+	}
+}
