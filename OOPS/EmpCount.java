fe1a189a3a8ce036793795b6b2a94787cdecf779
Athmakuri_Akshara <107808096+AksharaAthmakuri@users.noreply.github.com>
2025-07-25 21:20:56 +0530

    Create EmpCount.java

---
+++ OOPS/EmpCount.java
@@ -0,0 +1,17 @@
+package javaoops_polymorphism;
+public class Task2 {
+	static int count=0;
+	Task2()
+	{
+		count++;
+		System.out.println(count);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Task2 t=new Task2();
+		Task2 t1=new Task2();
+		Task2 t2=new Task2();
+		Task2 t3=new Task2();
+		Task2 t4=new Task2();
+	}
+}
