efddea97cea7b0bef184dcb99655abbb09304790
Athmakuri_Akshara <107808096+AksharaAthmakuri@users.noreply.github.com>
2025-07-25 20:58:14 +0530

    Create Default_values.java

---
+++ Java_Fundamentals/Default_values.java
@@ -0,0 +1,26 @@
+package arraysjava;
+import java.util.Scanner;
+public class Task72 {
+		// TODO Auto-generated method stub
+		static byte a;
+		static short b;
+		static int c;
+		static long d;
+		static float e;
+		static double f;
+		static char ch;
+		static boolean x;
+		static void displayDefaults() {
+		System.out.println("default byte :" +a);
+		System.out.println("default short :" +b);
+		System.out.println("default int :" +c);
+		System.out.println("default long :" +d);
+		System.out.println("default float :" +e);
+		System.out.println("default double :" +f);
+		System.out.println("default byte :" +ch);
+		System.out.println("default byte :" +x);
+		 }
+		 public static void main(String[] args) {
+			displayDefaults();
+		}
+	}
