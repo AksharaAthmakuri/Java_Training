03c4145f87a67744f58080f2c84b25b8e67af5d9
Athmakuri_Akshara <107808096+AksharaAthmakuri@users.noreply.github.com>
2025-07-25 21:09:28 +0530

    Create Control_stm1.java

---
+++ Java_Fundamentals/Control_stm1.java
@@ -0,0 +1,21 @@
+package arraysjava;
+public class Task91 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=5;
+		int n2=10;
+		int n3=25;
+		if(n1>n2&&n1>n3)
+		{
+			System.out.println("the greatest number is :"+n1);
+		}
+		else if(n2>n1&&n2>n3)
+		{
+			System.out.println("the greatest number is :"+n2);
+		}
+		else
+		{
+			System.out.println("the greatest number is:"+n3);
+		}
+	}
+}
