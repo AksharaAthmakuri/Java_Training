43d28998318ccf38fceedc72a8840608e56fccc6
Athmakuri_Akshara <107808096+AksharaAthmakuri@users.noreply.github.com>
2025-07-25 20:34:22 +0530

    Create Keywords.java

---
+++ Java_Fundamentals/Keywords.java
@@ -0,0 +1,20 @@
+
+public class Task41 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=4;
+		if(x>5)
+		{
+			System.out.println("x is large");
+		}
+		else
+		{
+			System.out.println("x is not large");
+		}
+		
+		
+
+	}
+
+}
