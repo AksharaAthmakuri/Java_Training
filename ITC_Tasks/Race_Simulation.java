eea51a3f657e8d89161c9c9bdda3c932a781937d
Athmakuri_Akshara <107808096+AksharaAthmakuri@users.noreply.github.com>
2025-07-29 00:48:19 +0530

    Update Race_Simulation.java

--- ITC_Tasks/Race_Simulation.java
+++ ITC_Tasks/Race_Simulation.java
@@ -1,25 +1,28 @@
 package multithreadingtask;
-class AnimalRunner implements Runnable {
-    String name;
-    public AnimalRunner(String name) {
-        this.name = name;
-        new Thread(this, name).start();
-    }
-    public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(name + ": running..");
-            try {
-                Thread.sleep(rand.nextInt(1000));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+class Animal implements Runnable{
+	String name;
+	public Animal(String name)
+	{
+		this.name=name;
+		new Thread(this,name).start();
+	}
+	public void run() {
+		for(int i=1;i<=5;i++) {
+			System.out.println(name+": running");
+			try {
+				Thread.sleep(500);
+			}
+			catch(InterruptedException e)
+			{
+				e.getStackTrace();
+			}
+		}
+	}
 }
 public class Task11 {
-    public static void main(String[] args) {
-        new AnimalRunner("tortoise");
-        new AnimalRunner("rabbit");
-        new AnimalRunner("dog");
-    }
+	public static void main(String[] args) {
+	Animal a=new Animal("tortoise");
+	Animal b=new Animal("rabbit");
+	Animal c=new Animal("dog");
+	}
 }
