47eaa74e8a555ba98755fd5eb3eaa0fe82313012
Athmakuri_Akshara <107808096+AksharaAthmakuri@users.noreply.github.com>
2025-07-28 18:45:22 +0530

    Create Race_Simulation.java

---
+++ ITC_Tasks/Race_Simulation.java
@@ -0,0 +1,27 @@
+package multithreadingtask;
+import java.util.Random;
+class AnimalRunner implements Runnable {
+    String name;
+    Random rand = new Random();
+    public AnimalRunner(String name) {
+        this.name = name;
+        new Thread(this, name).start();
+    }
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(name + ": running..");
+            try {
+                Thread.sleep(rand.nextInt(1000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+public class Task11 {
+    public static void main(String[] args) {
+        new AnimalRunner("tortoise");
+        new AnimalRunner("rabbit");
+        new AnimalRunner("dog");
+    }
+}
