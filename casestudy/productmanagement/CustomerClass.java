214f39ed895b2d0c215519eef9b4aceb812133ba
Athmakuri_Akshara <107808096+AksharaAthmakuri@users.noreply.github.com>
2025-07-29 18:04:12 +0530

    Create CustomerClass.java

---
+++ casestudy/productmanagement/CustomerClass.java
@@ -0,0 +1,9 @@
+package productmanagement;
+public class CustomerClass {
+	public int CID;
+	public String CName;
+	public CustomerClass(int CID,String CName) {
+		this.CID=CID;
+		this.CName=CName;
+	}
+}
